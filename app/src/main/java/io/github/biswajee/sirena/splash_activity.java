c821fde04820e3911b3406c5d2e4421a1711e3cd
Biswajee <roy.biswajeet161@gmail.com>
2018-07-27 00:12:22 +0530

    make splash activity

---
+++ app/src/main/java/io/github/biswajee/sirena/splash_activity.java
@@ -0,0 +1,30 @@
+package io.github.biswajee.sirena;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Biswajit Roy on 27-07-2018.
+ */
+
+public class splash_activity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        //getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(splash_activity.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}

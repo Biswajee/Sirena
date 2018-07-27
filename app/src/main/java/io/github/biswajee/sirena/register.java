abea0ba160584db79c894de8c278223348fc2853
Biswajee <roy.biswajeet161@gmail.com>
2018-07-28 02:06:34 +0530

    Update register

--- app/src/main/java/io/github/biswajee/sirena/register.java
+++ app/src/main/java/io/github/biswajee/sirena/register.java
@@ -1,11 +1,27 @@
 package io.github.biswajee.sirena;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Biswajit Roy on 28-07-2018.
  */
 
 public class register extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+        Button reg_button = (Button)findViewById(R.id.btn_reg);
+        reg_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
 
+    }
 }

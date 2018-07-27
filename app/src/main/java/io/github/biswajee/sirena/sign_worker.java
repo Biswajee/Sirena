20a829e2c6d5ebaacbaa064016870f11ce619e8c
Biswajee <roy.biswajeet161@gmail.com>
2018-07-27 15:10:27 +0530

    Update Sign_worker

---
+++ app/src/main/java/io/github/biswajee/sirena/sign_worker.java
@@ -0,0 +1,12 @@
+package io.github.biswajee.sirena;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Biswajit Roy on 27-07-2018.
+ */
+
+public class sign_worker extends AppCompatActivity {
+    
+}
+

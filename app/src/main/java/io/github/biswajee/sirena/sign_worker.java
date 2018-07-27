ee7bdb80e3bbabb21701d68eef415c3cc72fc588
Biswajee <roy.biswajeet161@gmail.com>
2018-07-27 22:47:03 +0530

    working on sign_screen

--- app/src/main/java/io/github/biswajee/sirena/sign_worker.java
+++ app/src/main/java/io/github/biswajee/sirena/sign_worker.java
@@ -1,9 +1,6 @@
 package io.github.biswajee.sirena;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,24 +11,12 @@ import com.google.firebase.auth.FirebaseUser;
  */
 
 public class sign_worker extends AppCompatActivity {
-    FirebaseAuth mAuth;
+    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_screen);
-        mAuth = FirebaseAuth.getInstance();
-    }
-
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        Intent homeIntent = new Intent(sign_worker.this, MainActivity.class);
-        startActivity(homeIntent);
-
     }
 
 }

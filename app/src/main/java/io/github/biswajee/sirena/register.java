bbf6a7e874459ae68e0c98cf3da2da86bdad4632
Biswajee <roy.biswajeet161@gmail.com>
2018-07-28 07:18:41 +0530

    Register part...done

--- app/src/main/java/io/github/biswajee/sirena/register.java
+++ app/src/main/java/io/github/biswajee/sirena/register.java
@@ -2,13 +2,19 @@ package io.github.biswajee.sirena;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Created by Biswajit Roy on 28-07-2018.
@@ -29,7 +35,7 @@ public class register extends AppCompatActivity {
 
         reg_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(final View v) {
                 if(password.getText().toString().equals(pass_again.getText().toString())!= true){
                     Snackbar.make(v, "Passwords do not match !",Snackbar.LENGTH_SHORT).show();
                     return;
@@ -45,7 +51,22 @@ public class register extends AppCompatActivity {
                     return;
                 }
 
-
+                mAuth = FirebaseAuth.getInstance();
+                mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(register.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(register.this,"Successfully Registered",Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent homeIntent = new Intent(register.this, MainActivity.class);
+                            startActivity(homeIntent);
+                            finish();
+                        }
+                        else{
+                            Snackbar.make(v, "Authentication failed.", Snackbar.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
             }
         });

a1bd53b78d48e350361d12bc6518044abb3b483d
Biswajee <roy.biswajeet161@gmail.com>
2018-08-05 01:13:20 +0530

    Improving Sign out

--- app/src/main/java/io/github/biswajee/sirena/MainActivity.java
+++ app/src/main/java/io/github/biswajee/sirena/MainActivity.java
@@ -1,5 +1,6 @@
 package io.github.biswajee.sirena;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -258,11 +260,22 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_send) {
 
         } else if (id == R.id.logout){
-            Toast.makeText(getApplicationContext(),"You clicked logout", Toast.LENGTH_SHORT).show();
-            getSharedPreferences("Avatar", MODE_PRIVATE).edit().clear().commit();
-            getSharedPreferences("Login", MODE_PRIVATE).edit().clear().commit();
-            FirebaseAuth.getInstance().signOut();
-            finish();
+            AlertDialog.Builder logoutBuilder = new AlertDialog.Builder(this);
+            logoutBuilder.setCancelable(true);
+            logoutBuilder.setMessage("Logout and exit application ?");
+            logoutBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(getApplicationContext(),"You clicked logout", Toast.LENGTH_SHORT).show();
+                    getSharedPreferences("Avatar", MODE_PRIVATE).edit().clear().commit();
+                    getSharedPreferences("Login", MODE_PRIVATE).edit().clear().commit();
+                    FirebaseAuth.getInstance().signOut();
+                    finish();
+                }
+            });
+            AlertDialog logoutDialog = logoutBuilder.create();
+            logoutDialog.setCanceledOnTouchOutside(true);
+            logoutDialog.show();
         }
 
             DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
